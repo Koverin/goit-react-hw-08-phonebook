9cf727f7223cb14543cc354c0087ea27ffb57b57
Koverin <110396064+Koverin@users.noreply.github.com>
2023-04-11 19:30:37 +0000

    Deploying to gh-pages from @ Koverin/goit-react-hw-08-phonebook@7401cdc5ac60d0d2fcd7b98afd64f63ba597b982 🚀

---
+++ static/js/206.ede4f8d6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("h1",{children:"Contact book"}),(0,n.jsx)("p",{children:"Welcome to our app!"})]})}}}]);
+//# sourceMappingURL=206.ede4f8d6.chunk.js.map
